c97e0acfb23761b135b05c84033f5adca8c8606b
Root Lock <rootlock@Roots-MacBook-Air.local>
2024-09-24 15:28:23 -0600

    feat: ✨Add vim-tmux-plugin

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -14,3 +14,9 @@ opt.termguicolors = true
 opt.relativenumber = true
 opt.number = true
 opt.cursorline = true
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
